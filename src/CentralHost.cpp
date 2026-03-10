02088830d6d917bfc173c97ddbd9cf3022bfb26d
LordOfTheNeverThere <miguel.dinis.s@tecnico.ulisboa.pt>
2026-03-10 14:05:13 +0000

    Saving

---
+++ src/CentralHost.cpp
@@ -0,0 +1,21 @@
+//
+// Created by miguel on 3/2/26.
+//
+
+#include "../include/networkMapper/CentralHost.h"
+
+
+
+class CentralHost : public Host{
+
+    private:
+
+    public:
+    void createNetworkMap() {
+
+
+    }
+    void getHostInformation() {
+
+    }
+};
--- src/Map.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by miguel on 2/27/26.
-//
-
-#include "Map.h"
